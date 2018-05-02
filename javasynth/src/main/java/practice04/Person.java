05f4ab7fc01349afd71555a9fa36dbb19d022b8d
shuhan2 <2472520650@qq.com>
2018-05-02 10:46:59 +0800

    practice04

--- javasynth/src/main/java/practice04/Person.java
+++ javasynth/src/main/java/practice04/Person.java
@@ -1,4 +1,32 @@
 package practice04;
 
 public class Person {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public  String name;
+    public Integer age;
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String introduce(){
+        String result = "My name is "+this.name+"."+" I am "+this.age+" years old.";
+        return  result;
+    }
+    public String basicIntroduce(){
+        String result = "My name is "+this.name+"."+" I am "+this.age+" years old. ";
+        return  result;
+    }
 }
