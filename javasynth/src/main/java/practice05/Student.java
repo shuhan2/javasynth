029673b30d1067c6145cd07d71ae38f1f1c6e68a
shuhan2 <2472520650@qq.com>
2018-05-02 10:54:58 +0800

    practice05

--- javasynth/src/main/java/practice05/Student.java
+++ javasynth/src/main/java/practice05/Student.java
@@ -1,4 +1,22 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public int klass;
+    public Student(String name,int age,int klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    public String introduce(){
+        Person person = new Person(name,age);
+        String result = person.introduce()+" I am a Student. "+"I am at Class "+this.klass+".";
+        return  result;
+    }
 }
--- javasynth/src/main/java/practice05/Worker.java
+++ javasynth/src/main/java/practice05/Worker.java
@@ -1,4 +1,13 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name,int age){
+        super(name,age);
+
+    }
+    public String introduce(){
+        Person person = new Person(name,age);
+        String result = person.introduce()+" I am a Worker. I have a job.";
+        return  result;
+    }
 }
