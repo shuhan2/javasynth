ba37f0e1f18c51d364e18ef3a3d0ba11344a9f60
shuhan2 <2472520650@qq.com>
2018-05-02 10:36:46 +0800

    practice03

--- javasynth/src/main/java/practice03/Student.java
+++ javasynth/src/main/java/practice03/Student.java
@@ -1,4 +1,21 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public int klass;
+    public Student(String name,int age,int klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    public String introduce(){
+        String result = "I am a Student. "+"I am at Class "+this.klass+".";
+        return  result;
+    }
 }
