596da9ade24a8e879898a91afdaa61f49e984c3e
shuhan2 <2472520650@qq.com>
2018-05-02 16:24:21 +0800

    practice08

--- javasynth/src/main/java/practice08/Klass.java
+++ javasynth/src/main/java/practice08/Klass.java
@@ -1,4 +1,34 @@
 package practice08;
 
 public class Klass {
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int number;
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public Student leader;
+    public Klass(int num){
+        this.number = num;
+
+    }
+    public String getDisplayName(){
+        return "Class "+this.number;
+    }
+    public void assignLeader(Student student){
+       leader = student;
+    }
+
 }
--- javasynth/src/main/java/practice08/Student.java
+++ javasynth/src/main/java/practice08/Student.java
@@ -1,4 +1,30 @@
 package practice08;
 
-public class Student {
+public class Student extends  Person{
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass klass;
+    public Student(int id,String name,int age,Klass klass){
+        super(id,name,age);
+        this.klass=klass;
+    }
+    public String introduce(){
+        Person person = new Person(id,name,age);
+        String result;
+        if (this.klass.getLeader()!=null&&this.klass.getLeader().name.equals(person.name)) {
+             result = person.introduce() + " I am a Student. " + "I am Leader of Class " + this.klass.number + ".";
+        }
+        else{
+            result = person.introduce()+" I am a Student. "+"I am at Class "+this.klass.number+".";
+        }
+        return  result;
+    }
+
+
 }
