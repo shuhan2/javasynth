48bfcda6440568430aa560f97ef2266f838cb0dd
shuhan2 <2472520650@qq.com>
2018-05-02 11:28:15 +0800

    practice06

--- javasynth/src/main/java/practice06/Teacher.java
+++ javasynth/src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public int klass;
+    public Teacher(String name,int age,int klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    public Teacher(String name,int age){
+        super(name,age);
+    }
+    public String introduce(){
+        Person person = new Person(name,age);
+        String result = "";
+        if (klass==0) {
+
+            result = person.introduce() + " I am a Teacher. I teach No Class." ;
+        }
+        else {
+            result = person.introduce() + " I am a Teacher. I teach Class " + this.klass + ".";
+        }
+        return  result;
+    }
 }
